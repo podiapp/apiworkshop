3163058a2859d4dcfc91e3d3d25b9d797c77522b
Guilherme Figueira <guilherme@podiapp.com.br>
2022-06-28 11:13:08 -0300

    feat: added crud for gift

---
+++ src/ApiWorkshop.Application/Domain/Responses/GiftResponse.cs
@@ -0,0 +1,27 @@
+﻿using ApiWorkshop.Application.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiWorkshop.Application.Domain.Responses;
+
+public class GiftResponse
+{
+    public GiftResponse(Guid id, string name, string photo, string description, int quantity, Status status)
+    {
+        Id = id;
+        Name = name;
+        Photo = photo;
+        Description = description;
+        Quantity = quantity;
+        Status = status;
+    }
+
+    public Guid Id { get; set; }
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+    [StringLength(300)]
+    public string Photo { get; set; } = string.Empty;
+    [StringLength(500)]
+    public string Description { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public Status Status { get; set; }
+}
--- tests/ApiWorkshop.Application.Tests/GiftServiceTests.cs
+++ tests/ApiWorkshop.Application.Tests/GiftServiceTests.cs
@@ -1,6 +1,7 @@
 using ApiWorkshop.Application.Domain.Entities;
 using ApiWorkshop.Application.Domain.Interfaces;
 using ApiWorkshop.Application.Domain.Requests;
+using ApiWorkshop.Application.Domain.Responses;
 using ApiWorkshop.Application.Services;
 
 namespace ApiWorkshop.Application.Tests
@@ -18,17 +19,69 @@ namespace ApiWorkshop.Application.Tests
 
 
         [Fact]
-        public async Task InsertGift_ShouldCreateGift_WhenAllValid()
+        public async Task CreateGift_ShouldCreateGift_WhenAllValid()
         {
             // Arrange
             GiftRequest giftRequest = _fixture.Build<GiftRequest>().Create();
 
             // Act
-            var response = await _sut.Insert(giftRequest);
+            var response = await _sut.Create(giftRequest);
 
             // Assert
             await _giftBaseRepository.Received(1).SaveChangesAsync();
+            _giftBaseRepository.Received(1).Insert(Arg.Any<Gift>());
             giftRequest.Name.Equals(response.Name);
+        }
+        [Fact]
+        public async Task UpdateGift_ShouldUpdateGift_WhenAllValid()
+        {
+            // Arrange
+            GiftRequest giftRequest = _fixture.Build<GiftRequest>().Create();
+            Guid id = Guid.NewGuid();
+            var gift = _fixture.Build<Gift>().With(g => g.Id, id).Create();
+            _giftBaseRepository.Where(id).Returns(gift);
+
+            // Act
+            var response = await _sut.Update(id, giftRequest);
+
+            // Assert
+            await _giftBaseRepository.Received(1).SaveChangesAsync();
+            _giftBaseRepository.Received(1).Update(Arg.Any<Gift>());
+            giftRequest.Name.Equals(response.Name);
+        }
+
+        [Fact]
+        public async Task DeleteGift_ShouldDeleteGift_WhenAllValid()
+        {
+            // Arrange
+            GiftRequest giftRequest = _fixture.Build<GiftRequest>().Create();
+            Guid id = Guid.NewGuid();
+            var gift = _fixture.Build<Gift>().With(g => g.Id, id).Create();
+            _giftBaseRepository.Where(id).Returns(gift);
+
+            // Act
+            var response = await _sut.Delete(id, giftRequest);
+
+            // Assert
+            await _giftBaseRepository.Received(1).SaveChangesAsync();
+            await _giftBaseRepository.Received(1).Delete(id);
+        }
+
+        [Fact]
+        public async Task ReadGiftById_ShouldReadGift_WhenAllValid()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            var gift = _fixture.Build<Gift>().With(g => g.Id, id).Create();
+            _giftBaseRepository.Where(id).Returns(gift);
+
+            // Act
+            var response = await _sut.ReadById(id);
+
+            // Assert
+
+            await _giftBaseRepository.Received(1).Where(Arg.Any<Guid>());
+            gift.Name.Equals(response.Data?.Name);
         }
     }
 }
